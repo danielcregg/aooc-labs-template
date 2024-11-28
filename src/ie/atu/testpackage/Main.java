cb9e6d5d7922dbba34d26f9b6e9258261f88b85f
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-11-28 23:31:39 +0000

    Update and rename HelloWorld.java to Main.java

--- src/ie/atu/testpackage/Main.java
+++ src/ie/atu/testpackage/Main.java
@@ -1,9 +1,7 @@
 package ie.atu.testpackage;
 
-class HelloWorld
-{
-    public static void main(String []args)
-    {
+public class Main {
+    public static void main(String []args) {
         System.out.println("Hello, World!");
     }
 };
